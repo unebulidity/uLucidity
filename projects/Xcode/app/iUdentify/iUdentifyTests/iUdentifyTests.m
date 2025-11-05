a4bb8984de3afc5e47e48ccdd02e2db243d49d93
unebulidity <unebulidity@gmail.com>
2025-11-05 11:16:56 -0800

    updated uLucidity/source/uLucidity.

---
+++ projects/Xcode/app/iUdentify/iUdentifyTests/iUdentifyTests.m
@@ -0,0 +1,39 @@
+//
+//  iUdentifyTests.m
+//  iUdentifyTests
+//
+//  Created by John Boyd on 10/20/25.
+//  Copyright © 2025 ulucidity. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iUdentifyTests : XCTestCase
+
+@end
+
+@implementation iUdentifyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
